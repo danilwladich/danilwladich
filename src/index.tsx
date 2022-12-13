9ba6bb2c66c10b405de6ef371eb052d98849a67b
Daniel <frosbutte1@gmail.com>
2022-12-13 21:43:07 +0100

    indextsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 
 
@@ -13,8 +12,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <div className="wrapper">
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </div>
 );

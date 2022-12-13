0395816deb32bfcfd31bbe1e85bdc634ebbc4872
Daniel <frosbutte1@gmail.com>
2022-12-13 23:05:02 +0100

    style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,11 @@ export function Footer() {
 				<div className="container">
 					<div className="footer__row">
 						<div className="footer__copy">
-							<p>Daniel Władyczewski</p>
-							<p>frosbutte1@gmail.com</p>
-							<p>Warsaw 2022</p>
+							<p className='animated-item'>Daniel Władyczewski</p>
+							<p className='animated-item'>frosbutte1@gmail.com</p>
+							<p className='animated-item'>Warsaw 2022</p>
 						</div>
-						<nav className="footer__social">
+						<nav className="footer__social animated-item">
 							<a href="https://www.instagram.com/danil_wladich/" target='_blank' rel="noreferrer" ><img src="img/icons/instagram.svg" alt="instagram" /></a>
 							<a href="https://www.facebook.com/danilwladich/" target='_blank' rel="noreferrer" ><img src="img/icons/facebook.svg" alt="facebook" /></a>
 							<a href="https://github.com/danilwladich" target='_blank' rel="noreferrer" ><img src="img/icons/github.svg" alt="github" /></a>
